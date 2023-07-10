2ff09c31fd13a40de323262c1e43089114a8be13
kirby-b <kirby.brodie66@gmail.com>
2023-07-10 18:44:52 -0400

    small rework
    changed a file name

---
+++ Math/math.py
@@ -0,0 +1,26 @@
+
+from BasicMathClass import BasicMath
+from InterestClass import Interest
+from AreaOfObjectClass import AreaOfObject
+end = False
+
+def main():
+    print("Welcome to my math program.")
+
+    while end == False: 
+        print("""Please input what kind of math you would like to do (use the numbers):
+        1. Interest
+        2. Basic math operations
+        3. Area of 2d shapes""")
+
+        inp = str.strip(input())
+        while inp.isdigit() == False:
+            print("Invalid input try again")
+            inp = str.strip(input())
+        if int(inp) == 1:
+            Interest.getInterest()
+        if int(inp) == 2:
+            BasicMath.math()
+        if int(inp) == 3:
+            AreaOfObject.measuring()
+main()
