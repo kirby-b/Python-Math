dcfcd1f1ea665c981d66eb3b90fff6c81b852bd4
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-12 11:38:52 -0500

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -1,28 +1,31 @@
-
+#Imports the classes from the other files.
 from BasicMathClass import BasicMath
 from InterestClass import Interest
 from AreaOfObjectClass import AreaOfObject
-end = False
 
 def main():
     print("Welcome to my math program.")
 
-    while end == False: 
-        print("""Please input what kind of math you would like to do (use the numbers):
-        1. Interest
-        2. Basic math operations
-        3. Area of 2d shapes""")
-        #Gets which math program to run and runs it
-        inp = str.strip(input())
-        while inp.isdigit() == False:
-            print("Invalid input try again")
+    while True: 
+        try: #Tries to ask for input
+            print("""Please input what kind of math you would like to do (use the numbers):
+            1. Interest
+            2. Basic math operations
+            3. Area of 2d shapes""")
+            #Gets which math program to run and runs it
             inp = str.strip(input())
-        if int(inp) == 1:
-            Interest.getInterest()
-        if int(inp) == 2:
-            BasicMath.math()
-        if int(inp) == 3:
-            AreaOfObject.measuring()
+            #Validates the input
+            while inp.isdigit() == False:
+                print("Invalid input try again")
+                inp = str.strip(input())
+            if int(inp) == 1:
+                Interest.getInterest()
+            elif int(inp) == 2:
+                BasicMath.math()
+            elif int(inp) == 3:
+                AreaOfObject.measuring()
+        except(KeyboardInterrupt): #Catches a keyboard interupt
+            print("Shutting Down...")
             
 if __name__ == "__main__":
     main()
