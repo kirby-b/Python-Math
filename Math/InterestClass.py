37cd7bfcf5bb525510bef7d814134b4a4bf74fef
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-12 11:58:50 -0500

    Update InterestClass.py

--- Math/InterestClass.py
+++ Math/InterestClass.py
@@ -1,24 +1,29 @@
 class Interest:
-    
     def getInterest():
         initial = 0
         rate = 0
         time = 0
-
-        while initial <= 0:
-            initial = float(input("Enter the initial amount: "))
-            if initial <= 0:
+        #Initializes values and asks for inputs
+        initial = input("Enter the initial amount: "))
+        rate = input("Enter the interest rate: "))
+        time = input("Enter the time in years(round): "))
+        
+        while True: # Validation
+            if initial <= 0 or not is_float(initial):
                 print("Inital amount can not be less than or equal to zero")
-
-        while rate <= 0:
-            rate = float(input("Enter the interest rate: "))
-            if rate <= 0:
+                initial = float(input("Enter the initial amount: "))
+            if rate <= 0 or not is_float(initial):
                 print("Interest rate can not be less than or equal to zero")
-
-        while time <= 0:
-            time = int(input("Enter the time in years: "))
-            if time <= 0:
+                rate = float(input("Enter the interest rate: "))
+            if time <= 0 or not time.isnumeric():
                 print("Time passed can not be less than or equal to zero")
+                time = int(input("Enter the time in years(round): "))
         #Gets all values and calculates interest 
-        total = initial * pow((1 + rate / 100), time)
+        total = float(initial) * pow((1 + float(rate) / 100), int(time))
         print(f"Balance after {time} year/s: ${total:.2f}\n")
+    def is_float(string):#Custom function I made because using isinstance required me to add more code, in the end this was less lines.
+        try:
+            float(string)#Turn the input into a float. If it is not a number or float, it will throw an exception
+            return True
+        except ValueError: #On exception it will return false
+            return False
