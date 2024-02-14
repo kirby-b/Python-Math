d8e0e6aa2c3773e8b1c53bb9b1cb8c613b3e309e
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-14 11:20:22 -0500

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -8,16 +8,15 @@ def main():
 
     while True: 
         try: #Tries to ask for input
-            print("""Please input what kind of math you would like to do (use the numbers):
+            inp = str.strip(input("""Please input what kind of math you would like to do (use the numbers):
             1. Interest
             2. Basic math operations
-            3. Area of 2d shapes""")
+            3. Area of 2d shapes"""))
             #Gets which math program to run and runs it
-            inp = str.strip(input())
-            #Validates the input
-            while inp.isdigit() == False:
+            while inp.isdigit() == False or inp not in ["1", "2", "3"]:
                 print("Invalid input try again")
                 inp = str.strip(input())
+                #Validates the input
             if int(inp) == 1:
                 Interest.getInterest()
             elif int(inp) == 2:
