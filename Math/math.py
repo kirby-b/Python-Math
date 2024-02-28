a98d0e68e1ea2a62fcb58a050219af628d07b3ae
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:40:21 -0500

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -1,30 +1,32 @@
-#Imports the classes from the other files.
-from BasicMathClass import BasicMath
-from InterestClass import Interest
-from AreaOfObjectClass import AreaOfObject
+# Imports the classes from the other files.
+from BasicMathClass import do_math
+from InterestClass import get_interest
+from AreaOfObjectClass import measuring
+
 
 def main():
     print("Welcome to my math program.")
 
-    while True: 
-        try: #Tries to ask for input
+    while True:
+        try:  # Tries to ask for input
             inp = str.strip(input("""Please input what kind of math you would like to do (use the numbers):
             1. Interest
             2. Basic math operations
             3. Area of 2d shapes"""))
-            #Gets which math program to run and runs it
-            while inp.isdigit() == False or inp not in ["1", "2", "3"]:
+            # Gets which math program to run and runs it
+            while inp.isdigit() is False or inp not in ["1", "2", "3"]:
                 print("Invalid input try again")
                 inp = str.strip(input())
-                #Validates the input
+                # Validates the input
             if int(inp) == 1:
-                Interest.getInterest()
+                get_interest()
             elif int(inp) == 2:
-                BasicMath.math()
+                do_math()
             elif int(inp) == 3:
-                AreaOfObject.measuring()
-        except(KeyboardInterrupt): #Catches a keyboard interupt
+                measuring()
+        except KeyboardInterrupt:  # Catches a keyboard interrupt
             print("Shutting Down...")
-            
+
+
 if __name__ == "__main__":
     main()
