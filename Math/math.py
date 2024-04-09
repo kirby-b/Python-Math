a373caa2bd8ea14907859a5b5fe8b981ad142775
kirby-b <kirby.brodie66@gmail.com>
2024-04-09 16:28:55 -0400

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -12,7 +12,7 @@ def main():
             inp = str.strip(input("""Please input what kind of math you would like to do (use the numbers):
             1. Interest
             2. Basic math operations
-            3. Area of 2d shapes"""))
+            3. Area of 2d shapes\n"""))
             # Gets which math program to run and runs it
             while inp.isdigit() is False or inp not in ["1", "2", "3"]:
                 print("Invalid input try again")
