1f6e451441a803950c0b80ef202db77ac03de426
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-12-08 14:34:50 -0500

    Updated comments in main
    They should be more helpful now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Imports the classes from the other files.
+# Imports my math class files so their functions can be used.
 from BasicMath import do_math
 from Interest import get_interest
 from AreaOfObject import measuring
@@ -8,12 +8,12 @@ def main():
     print("Welcome to my math program.")
 
     while True:
-        try:  # Tries to ask for input
+        try:  # Prompts the user so they know what their choices are and how to respond.
             inp = str.strip(input("""Please input what kind of math you would like to do (use the numbers):
             1. Interest
             2. Basic math operations
             3. Area of 2d shapes\n"""))
-            # Gets which math program to run and runs it
+            # Gets the users input so the program know what to run.
             while inp.isdigit() is False or inp not in ["1", "2", "3"]:
                 print("Invalid input try again")
                 inp = str.strip(input())
@@ -24,7 +24,7 @@ def main():
                 do_math()
             elif int(inp) == 3:
                 measuring()
-        except KeyboardInterrupt:  # Catches a keyboard interrupt
+        except KeyboardInterrupt:  # Catches a keyboard interrupt so the user has a way to easily stop the program
             print("Shutting Down...")
 
 
